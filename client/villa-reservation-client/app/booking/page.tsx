04742fb61b0307112416ca02c3f49c991e2b3bf7
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2023-12-30 22:29:51 +0700

    feat : add placeholder for booking page

---
+++ client/villa-reservation-client/app/booking/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "../components/NavigationBar";
+
+function BookingPage() {
+  return (
+    <>
+      <NavigationBar />
+      <div>BookingPage</div>
+    </>
+  );
+}
+
+export default BookingPage;
