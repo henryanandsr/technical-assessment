4de01612b4f52755a65f8f26349fbc1094fee5d7
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2023-12-30 22:30:30 +0700

    feat : add simple villa listing and search bar

---
+++ client/villa-reservation-client/app/components/Villa/VillaListing.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+interface Villa {
+  id: string;
+  name: string;
+  short_description: string;
+  price: number;
+  images: { filename: string; aliases: string[]; data: { data: number[] } }[];
+}
+
+function VillaListing() {
+  const [villas, setVillas] = useState<Villa[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const uri = process.env.SERVER_URL + "/api/villa";
+
+  useEffect(() => {
+    const fetchVillas = async () => {
+      try {
+        const res = await axios.get(uri, { withCredentials: true });
+        setVillas(res.data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching villas:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchVillas();
+  }, []);
+
+  const displayImage = (imageData: number[]) => {
+    const uint8Array = new Uint8Array(imageData);
+    const base64String = btoa(
+      String.fromCharCode.apply(null, Array.from(uint8Array))
+    );
+    return `data:image/png;base64,${base64String}`;
+  };
+
+  const filteredVillas = villas.filter((villa) =>
+    villa.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4">Villa Listings</h1>
+      {/* Search Bar */}
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="border border-gray-300 px-4 py-2 rounded-md"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-8">
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {filteredVillas.map((villa) => (
+              <div
+                key={villa.id}
+                className="bg-white rounded-md overflow-hidden shadow-lg"
+              >
+                {villa.images && villa.images.length > 0 && (
+                  <img
+                    src={displayImage(villa.images[0].data.data)}
+                    alt={villa.name}
+                    className="w-full h-48 object-cover"
+                  />
+                )}
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">{villa.name}</h2>
+                  <p className="text-gray-700 mb-2">
+                    {villa.short_description}
+                  </p>
+                  <p className="text-blue-500 font-bold">
+                    ${villa.price}/night
+                  </p>
+                  <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">
+                    Book Now
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default VillaListing;
---
+++ client/villa-reservation-client/app/villa/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationBar from '../components/NavigationBar'
+import VillaListing from '../components/Villa/VillaListing'
+
+function VillaPage() {
+  return (
+    <>
+        <NavigationBar />
+        <VillaListing />
+    </>
+  )
+}
+
+export default VillaPage
