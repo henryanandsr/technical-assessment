74d2e9958e25ef524d67b9552d2f6d99fe6995b7
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2023-12-30 21:19:02 +0700

    feat : add item for logged in user

--- client/villa-reservation-client/app/components/NavigationBar.tsx
+++ client/villa-reservation-client/app/components/NavigationBar.tsx
@@ -1,4 +1,41 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Cookies from "js-cookie";
+
 function NavigationBar() {
+  const router = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    // Check if the user is logged in based on the presence of the access token
+    const serverUrl = process.env.SERVER_URL + "/api/info";
+    const accessToken = Cookies.get("accessToken");
+    if (accessToken) {
+      // If the access token is present, fetch user info
+      axios.get(serverUrl, {withCredentials: true})
+        .then((response) => {
+          const res = response.data;
+          setName(res.data.name);
+          setLoggedIn(true);
+        })
+        .catch((error) => {
+          setLoggedIn(false);
+        });
+    } else {
+      setLoggedIn(false);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    // Implement the logout logic (clear the access token, redirect, etc.)
+    localStorage.removeItem("accessToken");
+    setLoggedIn(false);
+    router.push("/login");
+  };
+
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex items-center justify-between">
@@ -7,8 +44,21 @@ function NavigationBar() {
 
         {/* Navigation Links */}
         <div className="space-x-4">
-          <a href="/login" className="text-white hover:text-gray-300">Sign In</a>
-          <a href="/register" className="text-white hover:text-gray-300">Sign Up</a>
+          {loggedIn ? (
+            <>
+              <a href="/villa" className="text-white hover:text-gray-300">Villa</a>
+              <a href="/booking" className="text-white hover:text-gray-300">Booking</a>
+              <span className="text-white">Welcome, {name}</span>
+              <button onClick={handleLogout} className="text-white hover:text-gray-300 cursor-pointer">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <a href="/login" className="text-white hover:text-gray-300">Sign In</a>
+              <a href="/register" className="text-white hover:text-gray-300">Sign Up</a>
+            </>
+          )}
         </div>
       </div>
     </nav>
