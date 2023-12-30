a57727a12613cb897bb7881fea6d1bf20112eb87
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2023-12-30 21:19:27 +0700

    feat : add hero image in landing page

--- client/villa-reservation-client/app/page.tsx
+++ client/villa-reservation-client/app/page.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 import NavigationBar from "./components/NavigationBar";
 import ListOfVilla from "./components/Homepage/ListOfVilla";
+import heroImage from "./assets/hero.jpg";
 
 const Home = () => {
   return (
     <>
       <NavigationBar />
-      <main className="flex flex-col items-center justify-between text-black">
-        <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
-          <p className="text-lg text-gray-600">
-            Explore amazing features and discover new possibilities.
-          </p>
-        </div>
-
         <img
-          src="https://placekitten.com/1200/600"
+          src={heroImage.src}
           alt="Beautiful Landscape"
-          className="mb-8 rounded-md shadow-lg"
+          className="mb-8 rounded-md shadow-lg w-full"
         />
         < ListOfVilla />
-      </main>
     </>
   );
 };
