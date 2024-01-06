317c18be1da935c628967799c297d68d628b1735
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2024-01-06 18:05:47 +0700

    fix : add city, countries and search controller

--- server/controller/villa.controller.ts
+++ server/controller/villa.controller.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import "dotenv/config";
+import { Prisma } from "@prisma/client";
+import { Amenity } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -15,6 +17,8 @@ export const createVilla = async (req: Request, res: Response) => {
       latitude,
       address,
       amenities,
+      city,
+      country,
     } = req.body;
 
     const parsedPrice = parseInt(price);
@@ -30,7 +34,6 @@ export const createVilla = async (req: Request, res: Response) => {
       });
       return;
     }
-
     // Create Villa with associated image
     const createdVilla = await prisma.villa.create({
       data: {
@@ -41,7 +44,11 @@ export const createVilla = async (req: Request, res: Response) => {
         longitude: parsedLongitude,
         latitude: parsedLatitude,
         address,
-        amenities: parsedAmenities,
+        city,
+        country,
+        amenities: {
+          set: parsedAmenities,
+        },
         images: {
           create: {
             data: Buffer.from(req.file.buffer),
@@ -74,6 +81,119 @@ export const getVilla = async (req: Request, res: Response) => {
       status: "success",
       message: "Villa retrieved successfully",
       data: villa,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export const getVillaBySearchFilterAndPage = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const {
+      search,
+      city,
+      country,
+      priceLow,
+      priceHigh,
+      amenities,
+      page,
+      itemsPerPage,
+    } = req.query;
+    const parsedPriceLow = parseInt(priceLow as string);
+    const parsedPriceHigh = parseInt(priceHigh as string);
+    const whereClause: Prisma.VillaWhereInput[] = [];
+
+    if (search) {
+      whereClause.push({
+        name: { contains: search as string, mode: "insensitive" },
+      });
+    }
+    if (city) {
+      whereClause.push({ city: { contains: city as string } });
+    }
+    if (country) {
+      whereClause.push({ country: { contains: country as string } });
+    }
+    if (priceLow) {
+      whereClause.push({ price: { lte: parsedPriceLow } });
+    }
+    if (priceHigh) {
+      whereClause.push({ price: { gte: parsedPriceHigh } });
+    }
+    if (amenities) {
+      console.log(amenities);
+      const parsedAmenities: Amenity[] = (amenities as string)
+        .split(",")
+        .map((amenity) => amenity.trim()) as Amenity[];
+      console.log(parsedAmenities);
+      whereClause.push({ amenities: { hasSome: parsedAmenities } });
+    }
+    const villa = await prisma.villa.findMany({
+      where: {
+        AND: whereClause,
+      },
+      include: {
+        images: true,
+      },
+      skip: (parseInt(page as string) - 1) * parseInt(itemsPerPage as string),
+      take: parseInt(itemsPerPage as string),
+    });
+    res.status(200).json({
+      status: "success",
+      message: "Villa retrieved successfully",
+      data: villa,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export const getAllCountries = async (req: Request, res: Response) => {
+  try {
+    const countries = await prisma.villa.findMany({
+      select: {
+        country: true,
+      },
+    });
+    const uniqueCountries = countries.map((country) => country.country);
+    res.status(200).json({
+      status: "success",
+      message: "Countries retrieved successfully",
+      data: uniqueCountries,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export const getCitiesOfCountry = async (req: Request, res: Response) => {
+  try {
+    const { country } = req.params;
+    const cities = await prisma.villa.findMany({
+      where: {
+        country: country,
+      },
+      select: {
+        city: true,
+      },
+    });
+    const uniqueCities = cities.map((city) => city.city);
+    res.status(200).json({
+      status: "success",
+      message: "Cities retrieved successfully",
+      data: uniqueCities,
     });
   } catch (error: any) {
     res.status(500).json({
@@ -118,6 +238,8 @@ export const updateVilla = async (req: Request, res: Response) => {
       longitude,
       latitude,
       address,
+      city,
+      country,
       amenities,
     } = req.body;
 
@@ -133,6 +255,8 @@ export const updateVilla = async (req: Request, res: Response) => {
         longitude,
         latitude,
         address,
+        city,
+        country,
         amenities,
       },
     });
--- server/routes/villa.routes.ts
+++ server/routes/villa.routes.ts
@@ -3,39 +3,25 @@ import {
   createVilla,
   getVilla,
   getVillaById,
-  // getVillaImage,
+  getVillaBySearchFilterAndPage,
+  getAllCountries,
+  getCitiesOfCountry
 } from "../controller/villa.controller";
 import multer from "multer";
-import { GridFsStorage } from "multer-gridfs-storage";
 const crypto = require("crypto");
 const path = require("path");
 
 // const dbURL = process.env.DATABASE_URL || "";
 const storage = multer.memoryStorage();
-// const storage = new GridFsStorage({
-//   url: dbURL,
-//   file: (req: any, file: any) => {
-//     return new Promise((resolve, reject) => {
-//       crypto.randomBytes(16, (err: any, buf: any) => {
-//         if (err) {
-//           return reject(err);
-//         }
-//         const filename = buf.toString("hex") + path.extname(file.originalname);
-//         const fileInfo = {
-//           filename: filename,
-//           bucketName: "uploads",
-//         };
-//         resolve(fileInfo);
-//       });
-//     });
-//   },
-// });
+
 const upload = multer({ storage });
 const router = express.Router();
 
 router.post("/villa", upload.single("image"), createVilla);
 router.get("/villa", getVilla);
+router.get("/villa/search", getVillaBySearchFilterAndPage);
 router.get("/villa/:id", getVillaById);
-// router.get("/villa/img/:id", getVillaImage);
+router.get("/countries", getAllCountries);
+router.get("/countries/:country", getCitiesOfCountry);
 
 module.exports = router;
