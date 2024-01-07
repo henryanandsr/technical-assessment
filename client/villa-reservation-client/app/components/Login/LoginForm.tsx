949e595a74c1c7888302e929c773945c4164c709
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2024-01-07 13:51:57 +0700

    feat : add notification for login

--- client/villa-reservation-client/app/components/Login/LoginForm.tsx
+++ client/villa-reservation-client/app/components/Login/LoginForm.tsx
@@ -1,22 +1,34 @@
-"use client";
 import { handleLogin } from "@/app/utils/auth";
 import React, { useState } from "react";
-import Cookie from "js-cookie";
 import { useRouter } from "next/navigation";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showSuccessNotification, setShowSuccessNotification] = useState(false);
+  const [showErrorNotification, setShowErrorNotification] = useState(false);
   const router = useRouter();
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const res: any = await handleLogin(email, password);
       if (res.status === "success") {
-        router.push("/");
+        setShowSuccessNotification(true);
+        setTimeout(() => {
+          router.push("/");
+        }, 1000);
       }
     } catch (err) {
-      console.error(err);
+      setShowErrorNotification(true);
+
+      // Hide error notification after 3 seconds
+      setTimeout(() => {
+        setShowErrorNotification(false);
+      }, 3000);
     }
   };
 
@@ -24,6 +36,16 @@ function LoginForm() {
     <div className="max-w-md mx-auto mt-24 p-4 bg-white shadow-md rounded-md">
       <h2 className="text-2xl font-bold mb-4 text-primary">Login</h2>
       <form onSubmit={handleSubmit}>
+        {showSuccessNotification && (
+          <div className="mt-4 p-2 bg-green-200 text-green-800 rounded-md mb-2">
+            Login successful!
+          </div>
+        )}
+        {showErrorNotification && (
+          <div className="mt-4 p-2 bg-red-200 text-red-800 rounded-md mb-2">
+            Invalid email or password. Please try again.
+          </div>
+        )}
         <div className="mb-4">
           <label
             htmlFor="email"
@@ -49,16 +71,29 @@ function LoginForm() {
           >
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="mt-1 p-2 w-full border rounded-md"
-            placeholder="Input your password here"
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 p-2 w-full border rounded-md pr-10"
+              placeholder="Input your password here"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-1/2 right-2 transform -translate-y-1/3 -translate-x-1/3 text-sm text-gray-600 cursor-pointer"
+            >
+              {showPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon icon={faEye} />
+              )}
+            </button>
+          </div>
         </div>
         <button
           type="submit"
