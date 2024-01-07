bbf93358dfc971e26b1b001d7ea28b13ffba3ef8
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2024-01-07 14:03:11 +0700

    feat : add notification for register form

--- client/villa-reservation-client/app/components/Register/RegisterForm.tsx
+++ client/villa-reservation-client/app/components/Register/RegisterForm.tsx
@@ -1,12 +1,21 @@
-"use client";
+'use client';
 import React, { useState } from "react";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
+  const Router = useRouter();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showSuccessNotification, setShowSuccessNotification] = useState(false);
 
   const handleChange = (e: any) => {
     setFormData({
@@ -15,17 +24,41 @@ const RegisterForm = () => {
     });
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
+
+    // Check if password and confirm password match
+    if (formData.password !== formData.confirmPassword) {
+      setPasswordMatch(false);
+      setTimeout(() => {
+        setPasswordMatch(true);
+      }
+      , 2000);
+      return;
+    }
+
+    // Send registration request
     try {
       const url = `${process.env.SERVER_URL}/api/user`;
-      fetch(url, {
+      const response = await fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
+
+      if (response.ok) {
+        // Show success notification for successful registration
+        setShowSuccessNotification(true);
+        setTimeout(() => {
+          setShowSuccessNotification(false);
+        }, 3000);
+        Router.push("/login");
+      } else {
+        // Handle other registration errors here
+        console.error("Registration failed:", response.statusText);
+      }
     } catch (err: any) {
       console.error(err);
     }
@@ -35,6 +68,18 @@ const RegisterForm = () => {
     <div className="max-w-md mx-auto mt-24 p-4 bg-white shadow-md rounded-md">
       <h2 className="text-2xl font-bold mb-4 text-primary">Register</h2>
       <form onSubmit={handleSubmit}>
+        {showSuccessNotification && (
+          <div className="mt-4 p-2 bg-green-200 text-green-800 rounded-md mb-2">
+            Registration successful!
+          </div>
+        )}
+        {
+          !passwordMatch && (
+            <div className="mt-4 p-2 bg-red-200 text-red-800 rounded-md mb-2">
+              Passwords do not match!
+            </div>
+          )
+        }
         <div className="mb-4">
           <label
             htmlFor="name"
@@ -76,15 +121,60 @@ const RegisterForm = () => {
           >
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md"
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md pr-10"
+              placeholder="Enter your password"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-1/2 right-2 transform -translate-y-1/3 -translate-x-1/3 text-sm text-gray-600 cursor-pointer"
+            >
+              {showPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon icon={faEye} />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="confirmPassword"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Confirm Password
+          </label>
+          <div className="relative">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md pr-10"
+              placeholder="Confirm your password"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              className="absolute top-1/2 right-2 transform -translate-y-1/3 -translate-x-1/3 text-sm text-gray-600 cursor-pointer"
+            >
+              {showConfirmPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon icon={faEye} />
+              )}
+            </button>
+          </div>
         </div>
         <button
           type="submit"
