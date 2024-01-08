988f3dafacc8a31781b44cd39c91e3a2f2dec34f
Henry Anand <39207406+henryanandsr@users.noreply.github.com>
2024-01-08 13:53:28 +0700

    feat : add responsive style to villa listing

--- client/villa-reservation-client/app/components/Villa/VillaListing.tsx
+++ client/villa-reservation-client/app/components/Villa/VillaListing.tsx
@@ -4,8 +4,19 @@ import useAxiosPrivate from "@/app/utils/api";
 import Link from "next/link";
 import villaListing from "../../assets/villa-listing.jpg";
 import MultiSelectDropdown from "./MultiSelectDropdown";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import path from "path";
+import {
+  Button,
+  Card,
+  Input,
+  Select,
+  Typography,
+  Option,
+  CardHeader,
+  CardBody,
+} from "@material-tailwind/react";
 
 interface Villa {
   id: string;
@@ -120,14 +131,14 @@ function VillaListing() {
         const res = await axiosInstance.get(uri_cities + "/" + countryParams, {
           withCredentials: true,
         });
-        console.log("uri_cities", uri_cities + "/" + country)
-        console.log("set cities", res.data.data)
+        console.log("uri_cities", uri_cities + "/" + country);
+        console.log("set cities", res.data.data);
         setCities(new Set(res.data.data.sort()));
       } catch (error) {
         console.error("Error fetching villas:", error);
       }
-    }
-    console.log("use effect 2", countryParams)  
+    };
+    console.log("use effect 2", countryParams);
     if (countryParams) {
       fetchCities();
     }
@@ -201,92 +212,148 @@ function VillaListing() {
       router.push(pathName + "?" + params.toString());
     }
   };
-
+  const handleCityChange = (value: string | undefined) => {
+    if (value) {
+      setCity(value);
+    }
+  };
   return (
     <div className="">
       <div className="relative">
-        <img src={villaListing.src} alt="" />
+        <img
+          src={villaListing.src}
+          alt=""
+          className="mb-8 rounded-md shadow-lg w-full h-screen object-cover"
+        />
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
+        <div className="absolute hidden md:block top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
           Lorem Ipsum Dolor sir Amet
         </div>
-        <div className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 bg-white flex flex-col">
+        <Card
+          className="absolute top-1/2 md:top-2/3 md:top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 bg-white"
+          placeholder={"form"}
+          color="transparent"
+          shadow={false}
+        >
           <div className="">
-            <input
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="mb-2"
+              placeholder={"Villa Name"}
+            >
+              Villa Name
+            </Typography>
+            <Input
               type="text"
-              placeholder="Search by name..."
+              size="lg"
               value={searchQuery || ""}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="border border-gray-300 px-4 py-2 w-full"
+              className="focus:border-none"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              crossOrigin={undefined}
             />
           </div>
-          <div className="flex flex-row mt-2 space-x-4">
-            <div>
-              <label htmlFor="country" className="block">
-                Country
-              </label>
-              <select
-                name="country"
-                id="country"
-                value={country || ""}
+          <div className="flex flex-col space-y-2 lg:space-y-0 lg:flex-row mt-2 space-x-4">
+            <div className="w-full lg:w-auto">
+              <Select
                 onChange={(e) => {
-                  setCountry(e.target.value);
-                  setCity(null);
+                  setCountry(e as string);
+                  setCity((prevCity) => {
+                    if (prevCity !== null) {
+                      return null;
+                    }
+                    return prevCity;
+                  });
                 }}
-                className="border border-gray-300 px-4 py-2"
+                label="Select Country"
+                placeholder={""}
+                animate={{
+                  mount: { y: 0 },
+                  unmount: { y: 25 },
+                }}
               >
-                <option value="">Select a country</option>
                 {Array.from(countries).map((country: string, index) => (
-                  <option key={`country-${index}`} value={country}>
+                  <Option key={`country-${index}`} value={country}>
                     {country}
-                  </option>
+                  </Option>
                 ))}
-              </select>
+              </Select>
             </div>
-            <div>
-              <label htmlFor="city" className="block">
-                City
-              </label>
-              <select
-                name="city"
-                id="city"
-                value={city || ""}
-                onChange={(e) => setCity(e.target.value)}
-                className="border border-gray-300 px-4 py-2"
+            <div className="">
+              <Select
+                onChange={(e) => {
+                  setCountry(e as string);
+                  setCity((prevCity) => {
+                    if (prevCity !== null) {
+                      return null;
+                    }
+                    return prevCity;
+                  });
+                }}
+                label="Select Country"
+                placeholder={""}
+                animate={{
+                  mount: { y: 0 },
+                  unmount: { y: 25 },
+                }}
               >
-                <option value="">Select a city</option>
-                {Array.from(cities).map((city: string, index) => (
-                  <option key={`city-${index}`} value={city}>
-                    {city}
-                  </option>
+                {Array.from(countries).map((country: string, index) => (
+                  <Option key={`country-${index}`} value={country}>
+                    {country}
+                  </Option>
                 ))}
-              </select>
+              </Select>
             </div>
-            <div>
-              <label htmlFor="priceLow" className="block">
-                Price Low
-              </label>
-              <input
+            <div className="">
+              <Select
+                key={country}
+                onChange={handleCityChange}
+                label="Select City"
+                placeholder={""}
+                animate={{
+                  mount: { y: 0 },
+                  unmount: { y: 25 },
+                }}
+              >
+                {Array.from(cities).map((city: string, index) => (
+                  <Option key={`city-${index}`} value={city}>
+                    {city}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+            <div className="">
+              <Input
                 type="number"
-                placeholder="Search by price low..."
+                label="Price Low"
+                placeholder=""
                 value={priceLow || ""}
                 onChange={(e) => setPriceLow(parseInt(e.target.value))}
-                className="border border-gray-300 px-4 py-2 "
+                crossOrigin={undefined}
+                className="focus:border-none"
+                labelProps={{
+                  className: "before:content-none after:content-none ml-1 ",
+                }}
               />
             </div>
-            <div className="flex flex-col justify-end">
-              <label htmlFor="priceHigh" className="block">
-                Price High
-              </label>
-              <input
+            <div className="">
+              <Input
                 type="number"
-                placeholder="Search by price high..."
+                label="Price High"
+                placeholder=""
                 value={priceHigh || ""}
                 onChange={(e) => setPriceHigh(parseInt(e.target.value))}
-                className="border border-gray-300 px-4 py-2 "
+                className="focus:border-none"
+                crossOrigin={undefined}
+                labelProps={{
+                  className: "before:content-none after:content-none ml-1 ",
+                }}
               />
             </div>
-            <div className="flex flex-col justify-end">
+            <div className="">
               <MultiSelectDropdown
                 formFieldName="Amenities"
                 options={amenitiesOptions}
@@ -295,35 +362,32 @@ function VillaListing() {
               />
             </div>
           </div>
-          <button
+          <Button
             onClick={handleSubmit}
-            className="mt-2 py-2 w-full rounded-md bg-tertiary text-white"
+            className="mt-2 py-2 w-full lg:w-auto rounded-md bg-tertiary text-white"
+            placeholder={"Search"}
           >
             Search
-          </button>
-        </div>
+          </Button>
+        </Card>
       </div>
-      <div className="container mx-auto z-1 px-20 mt-5">
+      <div className="container mx-auto z-1 p-5 md:px-20 mt-5">
         <div className="mb-2 flex flex-row justify-end items-center">
-          <label htmlFor="itemsPerPage" className="mr-2">
-            Items per page:
-          </label>
-          <select
+          <Select
+            label="Items per page"
             id="itemsPerPage"
             value={searchParams.get("itemsPerPage") || "10"}
             onChange={(e) =>
               router.push(
-                pathName +
-                  "?" +
-                  createQueryString("itemsPerPage", e.target.value)
+                pathName + "?" + createQueryString("itemsPerPage", e as string)
               )
             }
-            className="border border-gray-300 px-2 py-1"
+            placeholder=""
           >
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="50">50</option>
-          </select>
+            <Option value="10">10</Option>
+            <Option value="20">20</Option>
+            <Option value="50">50</Option>
+          </Select>
         </div>
         <div className="grid grid-cols-1 gap-8">
           {loading ? (
@@ -331,20 +395,30 @@ function VillaListing() {
           ) : (
             <>
               {villas.map((villa, index) => (
-                <div
+                <Card
                   key={index}
-                  className="bg-white rounded-md overflow-hidden flex flex-row shadow-md h-[20vh]"
+                  className="bg-white rounded-md overflow-hidden flex flex-col md:flex-row shadow-md"
+                  placeholder={"villa"}
                 >
-                  <Link href={"villa/" + villa.id}>
-                    {villa.images && villa.images.length > 0 && (
-                      <img
-                        src={displayImage(villa.images[0].data.data)}
-                        alt={villa.name}
-                        className="h-full w-96 object-cover"
-                      />
-                    )}
-                  </Link>
-                  <div className="p-4 flex flex-row justify-between w-full items-center">
+                  <CardHeader
+                    color="blue"
+                    placeholder={"image"}
+                    className="relative h-56"
+                  >
+                    <Link href={"villa/" + villa.id}>
+                      {villa.images && villa.images.length > 0 && (
+                        <img
+                          src={displayImage(villa.images[0].data.data)}
+                          alt={villa.name}
+                          // className="w-full h-full md:w-96 md:h-auto object-cover"
+                        />
+                      )}
+                    </Link>
+                  </CardHeader>
+                  <CardBody
+                    className="p-4 flex flex-row justify-between w-full items-center"
+                    placeholder={""}
+                  >
                     <Link href={"villa/" + villa.id}>
                       <h2 className="text-xl font-bold mb-2">{villa.name}</h2>
                       <p className="text-gray-700 mb-2">
@@ -354,22 +428,22 @@ function VillaListing() {
                         ${villa.price}/night
                       </p>
                     </Link>
-                    <div className="px-2">
+                    <Button className="px-2 bg-tertiary" placeholder={""}>
                       <Link
                         href={`/villa/checkout/${villa.id}`}
-                        className="bg-tertiary text-white rounded-md px-3 py-2"
+                        // className="bg-tertiary text-white rounded-md px-3 py-2"
                       >
                         Book Now
                       </Link>
-                    </div>
-                  </div>
-                </div>
+                    </Button>
+                  </CardBody>
+                </Card>
               ))}
             </>
           )}
         </div>
-        <div className="flex justify-center mt-4 mb-4">
-          <button
+        <div className="flex justify-center mt-4 mb-4 items-center">
+          <Button
             onClick={() => {
               const currentPage = parseInt(searchParams.get("page") || "1");
               router.push(
@@ -379,11 +453,15 @@ function VillaListing() {
               );
             }}
             disabled={Number(searchParams.get("page") || 1) === 1}
+            placeholder={"Previous Page"}
+            className="bg-primary"
           >
-            Previous Page
-          </button>
-          <span className="mx-2">Page {searchParams.get("page") || 1}</span>
-          <button
+            <FaArrowLeft />
+          </Button>
+          <Typography className="mx-2" placeholder={"Page"}>
+            Page {searchParams.get("page") || 1}
+          </Typography>
+          <Button
             onClick={() => {
               const currentPage = parseInt(searchParams.get("page") || "1");
               router.push(
@@ -392,9 +470,14 @@ function VillaListing() {
                   createQueryString("page", (currentPage + 1).toString())
               );
             }}
+            placeholder={"Next Page"}
+            disabled={
+              villas.length < parseInt(searchParams.get("itemsPerPage") || "10")
+            }
+            className="bg-primary"
           >
-            Next Page
-          </button>
+            <FaArrowRight />
+          </Button>
         </div>
       </div>
     </div>
